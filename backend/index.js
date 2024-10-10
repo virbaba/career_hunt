c38e239e4d1402bd9f2a46acb969a307cf505b26
Virender Singh <virendersinghdev1@gmail.com>
2024-10-10 15:44:04 +0530

    Change into backend package.json

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,13 @@
-import express from 'express';
-import cookieParser from 'cookie-parser';
-import cors from 'cors';
-import dotenv from 'dotenv';
-import connectDB from './utils/db.js';
-import userRoute from './routes/user.route.js';
-import companyRoute from './routes/company.route.js';
-import jobRoute from './routes/job.route.js';
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./utils/db.js";
+import userRoute from "./routes/user.route.js";
+import companyRoute from "./routes/company.route.js";
+import jobRoute from "./routes/job.route.js";
 import applicationRoute from "./routes/application.route.js";
-import path from 'path';
+import path from "path";
 
 dotenv.config();
 const app = express();
@@ -16,12 +16,12 @@ const PORT = process.env.PORT;
 // Middleware for parsing
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());  // Call cookieParser correctly
+app.use(cookieParser()); // Call cookieParser correctly
 
 // CORS settings
 const corsOptions = {
-  origin: "http://localhost:5173", 
-  credentials: true
+  origin: "http://localhost:5173",
+  credentials: true,
 };
 app.use(cors(corsOptions));
 
@@ -33,14 +33,14 @@ const __dirname = path.resolve();
 // Use routers
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/company", companyRoute);
-app.use("/api/v1/job", jobRoute);  // Removed duplicate route
+app.use("/api/v1/job", jobRoute); // Removed duplicate route
 app.use("/api/v1/application", applicationRoute);
 
 // for deployment
-app.use(express.static(path.join(__dirname, '/frontend/dist')));
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
 });
 
 // Start the server
